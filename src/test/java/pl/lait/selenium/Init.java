85d4a29fb84c43c1b0973c92d2a9d402e9c5fdb7
JS <JS>
2019-06-09 14:51:34 +0200

    pierwsze testy z uzyciem junita

---
+++ src/test/java/pl/lait/selenium/Init.java
@@ -0,0 +1,33 @@
+package pl.lait.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Init {
+	
+	static WebDriver driver;
+	
+    public static WebDriver getDriver() {
+    	
+    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\E6420\\Downloads\\chromedriver.exe");
+    	
+    	driver = new ChromeDriver();
+    	driver.get("http://newtours.demoaut.com");
+    	
+    	return driver;
+    }
+    public static void sleep(int seconds){
+    	try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+	}
+   	/*public static void sleep(int seconds){
+    	try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }*/	
+    }
+}
