807d867ab04f175940276bd6f57e204b5a02fd5b
JS <JS>
2019-06-15 14:45:50 +0200

    wersja z nullem i ifem

--- src/test/java/pl/lait/selenium/Init.java
+++ src/test/java/pl/lait/selenium/Init.java
@@ -11,11 +11,13 @@ public class Init {
     	System.out.println("wewnątrz getDriver");
     	
     	System.setProperty("webdriver.chrome.driver", "C:\\Users\\E6420\\Downloads\\chromedriver.exe");
-    	
-    	driver = new ChromeDriver();
-    	driver.get("http://newtours.demoaut.com");
-    	
-    	return driver;
+    	if (driver == null) {
+    		driver = new ChromeDriver();
+        	driver.get("http://newtours.demoaut.com");
+        	return driver;
+    	} else {
+    		return driver;
+    	}
     }
     
     public static void quit() {
